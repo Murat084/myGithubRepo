a2ad7338610b0778bac9c119d80d87aa8338c8a1
Murat084 <turana19murad@gmail.com>
2023-06-04 18:43:47 +0300

    final

--- src/CalculaterRunner.java
+++ src/CalculaterRunner.java
@@ -2,6 +2,7 @@ public class CalculaterRunner {
     public static void main(String[] args) {
         Calculator claculator = new Calculator();
         claculator.add(4,6);
+        System.out.println(claculator.multiply(10, 5));
     }
 
 }
